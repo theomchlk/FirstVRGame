47315b3a32abb5e43d8f246354e6d6e87a1c0287
theomchlk <theomchlk@gmail.com>
2025-11-15 17:31:37 +0100

    Ajout du bouton reset pour le scoring
    Le bouton "Reset" en dessous du score reset le score lorsqu'un objet passe à travers

--- Assets/Script/ResetButton.cs
+++ Assets/Script/ResetButton.cs
@@ -5,7 +5,7 @@ public class ResetButton : MonoBehaviour
     [SerializeField]
     private TargetManager targetManager;
 
-    private void OnTriggerEnter(){
+    private void OnTriggerEnter(Collider other){
         targetManager.ResetScore();
     }
 }
